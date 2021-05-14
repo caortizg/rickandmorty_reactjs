781ecdcfeb3ae87a8a1f66e00573e0a8803ee8b3
Andres Ortiz <c-andres-1211@hotmail.com>
2021-05-14 08:05:33 -0500

    app base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Characters from './container/Characters'
+import { connect } from 'react-redux'
 
-function App() {
+
+
+function App(props) {
+  props.iniListCharacterAsync()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Characters/>
     </div>
   );
 }
 
-export default App;
+const mapState = (state) => ({
+    stateCharacter: state.character,
+})
+
+const mapDispatch = (dispatch) => ({
+    iniListCharacterAsync: () => dispatch.character.iniAsync(),
+})
+
+export default connect(
+    mapState,
+    mapDispatch
+)(App)
+
