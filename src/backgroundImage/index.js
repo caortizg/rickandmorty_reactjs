4b5c4e113ef05268bb68fa8fc312684b9ffaf096
Andres Ortiz <c-andres-1211@hotmail.com>
2021-05-14 07:44:05 -0500

    Imagenes

---
+++ src/backgroundImage/index.js
@@ -0,0 +1,33 @@
+import bgImg1 from './1.png'; 
+import bgImg2 from './2.jpg'; 
+import bgImg3 from './3.jpg'; 
+import bgImg4 from './4.jpg'; 
+import bgImg5 from './5.jpg'; 
+import bgImg6 from './6.jpg'; 
+import bgImg7 from './7.jpg'; 
+import bgImg8 from './8.png'; 
+import dots1 from './dots_1.png';
+import dots2 from './dots_2.png';
+import dots3 from './dots_3.png';
+import dots4 from './dots_4.png';
+import dots5 from './dots_5.png';
+import dead from './dead.png';
+
+
+export default [
+    bgImg1,
+    bgImg2,
+    bgImg3,
+    bgImg4,
+    bgImg5,
+    bgImg6,
+    bgImg7,
+    bgImg8
+];
+export {dots1};
+export {dots2};
+export {dots3};
+export {dots4};
+export {dots5};
+
+export {dead};
